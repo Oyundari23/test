507488fc39476dd2e6c8633b65ba39476c6a4dd3
Oyundari23 <dariaa0560@gmail.com>
2024-09-27 22:26:27 +0800

    f

--- e-commerce-web/src/app/Signup/page.tsx
+++ e-commerce-web/src/app/Signup/page.tsx
@@ -8,8 +8,35 @@ export default function Page() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [passConfirm, setPassConfirm] = useState("");
+  
 
   console.log({ name, email, passConfirm, password });
+  const lengthGreater = password.length >= 8;
+  const hasUppercase = /[A-Z]/.test(password); 
+  const hasLowercase = /[a-z]/.test(password); 
+  const hasNumber = /[0-9]/.test(password);
+  const hasSpecialChar = /[!@#$%^&*()_+{}|:"<>?]/.test(password);
+  const passwordAreSame = password === passConfirm && password !== '';
+  const emailIsValid = /[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}/.test(email);
+
+  const isValid = lengthGreater && hasUppercase && hasLowercase && hasNumber && hasSpecialChar && passwordAreSame && name.length > 3 && emailIsValid;
+//   function submit() {
+//     fetch ('https//backend/signup', {
+//       method: 'POST',
+//       body: JSON.stringify ( {
+//         name, 
+//         email, 
+//         password
+//       }), 
+// //headers
+//     }). then ((res)=> {
+// if (res.ok) {//success}
+// else {
+//   //error
+// }
+//     })
+//   }
+
 
   return (
     <div className="mx-auto">
@@ -27,6 +54,7 @@ export default function Page() {
           {" "}
           <input
             className="bg-white rounded-2xl border-[1px] border-[#E4E4E7]"
+            type="email"
             placeholder="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -36,6 +64,7 @@ export default function Page() {
           {" "}
           <input
             className="bg-white rounded-2xl border-[1px] border-[#E4E4E7]"
+            type="password"
             placeholder="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
@@ -45,13 +74,28 @@ export default function Page() {
           {" "}
           <input
             className="bg-white rounded-2xl border-[1px] border-[#E4E4E7]"
+            type="password"
             placeholder="password confirm"
             value={passConfirm}
             onChange={(e) => setPassConfirm(e.target.value)}
           ></input>
         </div>
+        <ul>
+          <li className={hasUppercase ? 'text-green-400' : 'text-red-400' }> Том үсэг орсон байх</li>
+          <li className={hasLowercase ? 'text-green-400' : 'text-red-400' }> Жижиг үсэг орсон байх</li>
+          <li className={hasNumber ? 'text-green-400' : 'text-red-400' }> Тоо орсон байх</li>
+          <li className={hasSpecialChar ? 'text-green-400' : 'text-red-400' }> Тэмдэгт орсон байх</li>
+          <li className={passwordAreSame ? 'text-green-400' : 'text-red-400' }> Нууц үг ижилхэн байх</li>
+          <li className={lengthGreater ? 'text-green-400' : 'text-red-400' }> 8 болон түүнээс дээш тэмдэгт байх</li>
+        </ul>
         <div className="[w-10]">
-          <Button className="bg-pink-200 text-black rounded-2xl">Sign-up</Button>
+          <Button 
+          // backend ruu yvuulahdaa onClick = {submit}
+            className="bg-pink-200 text-black rounded-2xl disabled:opacity-30 disabled:cursor-not-allowed"
+            disabled={!isValid}
+          >
+            Sign-up
+          </Button>
         </div>
       </div>
     </div>
