f09a93d6f8b2c1612fb355966a64a160c7e58245
Oyundari23 <dariaa0560@gmail.com>
2024-11-05 16:29:32 +0800

    11/05

--- e-commerce-service/src/router/AuthRouter.ts
+++ e-commerce-service/src/router/AuthRouter.ts
@@ -7,13 +7,11 @@ const authRouter = express.Router();
 authRouter
   .post("/register",register )
   .post("/login", login)
- 
+
+
 
 export { authRouter };
-// export { authRouter };
-// export { authRouter };
-// export { authRouter };
-// export { authRouter };
+
+// export { authRouter };export { authRouter };export { authRouter };
 
 
-// export { authRouter };export { authRouter };export { authRouter };export { authRouter };
